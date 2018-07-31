a13bc3a26aa479f9e36e2fc9ae2e36bce3b2f5eb
Sebastian Bird <sebbird10@gmail.com>
2018-07-31 20:43:30 +1000

    integrating Flask RESTful endpoints and SQLAlchemy

--- src/main.py
+++ src/main.py
@@ -1,28 +1,46 @@
 # coding=utf-8
 
-from .entities.entity import Session, engine, Base
-from .entities.example import Example
+from flask import Flask, jsonify, request
 
-# generate database schema
+from .entities.entity import Session, engine, Base
+from .entities.example import Example, ExampleSchema
+
+# creating the Flask application
+app = Flask(__name__)
+
+# if needed, generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-examples = session.query(Example).all()
+@app.route('/examples')
+def get_examples():
+    # fetching from the database
+    session = Session()
+    example_objects = session.query(Example).all()
 
-if len(examples) == 0:
-    # create and persist dummy exam
-    python_example = Example("SQLAlchemy Example", "This is a test example.", "script")
-    session.add(python_example)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = ExampleSchema(many=True)
+    examples = schema.dump(example_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(examples.data)
 
-    # reload exams
-    exams = session.query(Example).all()
 
-# show existing exams
-print('### Examples:')
-for example in examples:
-    print(f'({example.id}) {example.title} - {example.description}')
+@app.route('/examples', methods=['POST'])
+def add_example():
+    # mount example object
+    posted_example = ExampleSchema(only=('title', 'description'))\
+        .load(request.get_json())
+
+    example = Example(**posted_example.data, created_by="HTTP post request")
+
+    # persist example
+    session = Session()
+    session.add(example)
+    session.commit()
+
+    # return created example
+    new_example = ExampleSchema().dump(example).data
+    session.close()
+    return jsonify(new_example), 201
